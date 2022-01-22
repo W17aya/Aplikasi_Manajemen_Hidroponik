135805da87d420a5152d6a975a18d1ffb5466c1a
Eko Wijaya <ekowijayawindows@gmail.com>
2022-01-22 22:16:46 +0700

    still not working

--- grafik_laba.php
+++ grafik_laba.php
@@ -2,12 +2,38 @@
 include "connection.php"; //Panggil koneksi ke database terlebih dahulu
 
 echo "<table>
-      <tr><th>No</th><th>Nilai A</th><th>Nilai B</th><th>A + B</th></tr>";
-      $no=0;
+      <tr><th>No</th><th>Id Laba</th><th>Tanggal laba</th><th>Kode Tanaman</th></tr>";
+      $no=1;
 $sql=mysqli_query($con, "SELECT * FROM laba");
+$j = mysqli_query($con, "SELECT SUM (total_laba) AS (jumlah_a) FROM laba");
+while($s= mysqli_fetch_array($sql)){
+      ?>
+<tr>
 
-//Menghitung Total Nila A dan B dengan query
-$j = mysqli_query($con, "SELECT SUM(total_laba) AS (jumlah_a) FROM laba");
+
+
+<td><?php echo $no++; ?></td>
+				<td><?php echo $s['id_laba']; ?></td>
+				<td><?php echo $s['total_laba']; ?></td>
+				<td><?php echo $s['total_laba']; ?></td>
+
+				
+				<td>
+					
+				</td>
+			</tr>
+			<?php 
+		}
+		?>
+	</table>
+</body>
+</html>
+<!-- //Menghitung Total Nila A dan B dengan query -->
+<!-- $j = mysqli_query($con, "SELECT SUM(total_laba) AS (jumlah_a) FROM laba");
 echo "<tr><td>jumlah_a</td>";
 echo "</table>";
 ?>
+
+</tr>
+</body>
+</html> -->
